05b1ab2e57677a58bf6bde294cc7de5df03b039d
Andrew <andrew@alsmith.net>
2022-01-20 17:54:03 -0600

    tweak simple scan

--- simple_scan/simple_scan.h
+++ simple_scan/simple_scan.h
@@ -2,49 +2,30 @@
 
 struct ZStringScanner
 {
-    char * contents;
-    int iByte;
-    int iLine;
+	char * contents;
+	int iByte;
+	int iLine;
 };
 
 internal void
 InitScanner(ZStringScanner * scanner, char * contents)
 {
-    scanner->contents = contents;
-    scanner->iByte = 0;
-    scanner->iLine = 0;
+	scanner->contents = contents;
+	scanner->iByte = 0;
+	scanner->iLine = 0;
 }
-
-enum class Endianness : u8
-{
-    Nil = 0,
-    Little = Nil,
-
-    Big
-};
 
 struct StringScanner
 {
-    String contents;
-    int iByte;
-    int iLine;
-
-    Endianness endianness;
+	String contents;
+	int iByte;
+	int iLine;
 };
 
 internal void
 InitScanner(StringScanner * scanner, String contents)
 {
-    scanner->contents = contents;
-    scanner->iByte = 0;
-    scanner->iLine = 0;
+	scanner->contents = contents;
+	scanner->iByte = 0;
+	scanner->iLine = 0;
 }
-
-namespace SimpleScan
-{
-
-// TODO - come up with a better solution that would support NUL bytes...
-//  they are are totally valid in byte streams without necessarily ending the stream...
-static constexpr char end = '\0';
-}
-
